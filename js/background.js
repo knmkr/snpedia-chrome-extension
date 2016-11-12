2d02b1042e558437c2ae1d759b6a22fd103b5ddd
Kensuke Numakura <knmkr3gma@gmail.com>
2016-11-12 15:28:17 +0900

    pass query words

--- js/background.js
+++ js/background.js
@@ -1,12 +1,12 @@
 chrome.contextMenus.create({
     id: "snpedia",
-    title: "Search on SNPedia",
-    contexts: ["all"]
+    title: "Search on SNPedia: %s",
+    contexts: ["selection"]
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId == "snpedia") {
-        var q = 'aldh2+rs671';
+        var q = info.selectionText;
 
         chrome.tabs.create({
             url: "https://www.snpedia.com/index.php?title=Special%3ASearch&profile=default&fulltext=Search&search=" + q
